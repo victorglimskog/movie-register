9e9658cdbd5134f0662d393f48573cd3dbf16d1a
Victor Glimskog <victor.glimskog@gmail.com>
2017-10-17 21:16:49 +0200

    Fixed rest-sql.class to comply with linter standards

--- rest-sql.class.js
+++ rest-sql.class.js
@@ -2,61 +2,60 @@ const pm = require('promisemaker');
 const mysql = require('mysql');
 
 module.exports = class RestSql {
-
     static start(settings) {
         this.settings = settings;
         this.connectToSql();
         return (...args) => new RestSql(...args);
     }
 
-    static connectToSql(){
+    static connectToSql() {
         this.db = pm(
             mysql.createConnection(this.settings.dbCredentials),
             {
                 rejectOnErrors: this.settings.runtimeErrors,
                 mapArgsToProps: {
-                    query: ["rows", "fields"]
-                }
-            }
-        )
-    };
+                    query: ['rows', 'fields'],
+                },
+            },
+        );
+    }
 
-    constructor(req, res,next){
+    static async query(q, params) {
+        const result = await RestSql.db.query(q, params);
+        return result.rows;
+    }
 
+    constructor(req, res, next) {
         this.req = req;
         this.res = res;
-        this.next = next
+        this.next = next;
+        this.settings = RestSql.settings;
 
-        this.settings = RestSql.settings
-
-        if(this.settings.baseUrl.substr(-1) != '/'){
+        if (this.settings.baseUrl.substr(-1) !== '/') {
             this.settings.baseUrl += '/';
         }
+
         this.analyseUrl();
 
-        if(['get', 'post', 'put','delete'].includes(this.method)){
+        if (['get', 'post', 'put', 'delete'].includes(this.method)) {
             this[this.method]();
         }
     }
 
-    async query(query, params){
-        let result = await RestSql.db.query(query,params);
-        return result.rows;
-    }
-
-    analyseUrl(){
-        let url = this.req.url;
-        let method = this.req.method.toLowerCase();
-        let baseUrl = this.settings.baseUrl;
-        if(url.index(baseUrl) != 0){
+    analyseUrl() {
+        const { url } = this.req.url;
+        const method = this.req.method.toLowerCase();
+        const { baseUrl } = this.settings.baseUrl;
+        const idIndex = 1;
+        if (url.index(baseUrl) !== 0) {
             this.next();
             return;
         }
 
-        let urlParts = url.split(baseUrl, 2)[1].split('/')
+        const urlParts = url.split(baseUrl, 2)[1].split('/');
         this.table = urlParts[0].split(';').join('');
-        this.id = urlParts[1];
+        this.id = urlParts[idIndex];
         this.method = method;
         this.idColName = this.settings.idMap[this.table] || 'id';
     }
-}
+};
