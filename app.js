d29ca7b247f53d4af6600063da0fcd26e735172b
Victor Glimskog <victor.glimskog@gmail.com>
2017-10-17 20:32:55 +0200

    Changed linting rule indent to 4 spaces, added mysql and promisemaker as dependencies

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 const express = require('express');
+const RestSql = require('./rest-sql.class');
+
 const app = express();
-const RestSql = require('./rest-sql.class')
 
 app.use(RestSql.start({
     dbCredentials: {
-        host: "127.0.0.1",
-        user: "root",
-        password: "password",
-        database: "movieregister"
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'myskul',
+        database: 'movieregister2',
     },
     baseUrl: '/restapi',
     idMap: {},
-    runtimeErrors: false
+    runtimeErrors: false,
 }));
 
 app.use(express.static('./www'));
 
-app.listen(3000, function(){
-    console.log("Up and running at Tannhäuser Gate 3k");
+app.listen(3000, () => {
+    console.log('Up and running at Tannhäuser Gate 3k');
 });
