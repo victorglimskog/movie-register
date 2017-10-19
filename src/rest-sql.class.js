74d52e2b2dc953ae3d5b3c9825c51bc301bbd501
Kristoffer Annerfeldt <koffiman@me.com>
2017-10-19 14:00:05 +0200

    added async get function

--- src/rest-sql.class.js
+++ src/rest-sql.class.js
@@ -58,7 +58,37 @@ module.exports = class RestSql {
         this.method = method;
         this.idColName = this.settings.idMap[this.table] || 'id';
     }
-	
+
+    async get() {
+        // Query with or without ID
+        let result = await this.query(
+          'SELECT * FROM `' + this.table + '`' + (this.id ? ' WHERE ' + this.title + ' =?' : ''),
+          [this.id]
+        );
+
+
+       // If error
+        if (result.constructor === Error) {
+          this.res.status(500);
+        }
+
+        // No post with a id could be found
+        else if (this.id && result.length === 0) {
+          this.res.status(500);
+          return;
+        }
+
+
+        // From array to object
+        else if(this.id) {
+          result = result[0];
+        }
+
+       // Return the reslut
+        this.res.json(result);
+      }
+
+  
 	async post(){
     
 		// convert iso date strings like "2017-10-05T11:42:46.169Z" to mysql compatible date string
@@ -87,3 +117,4 @@ module.exports = class RestSql {
 	
 	}
 };
+
