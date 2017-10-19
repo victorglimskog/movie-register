0bff309c433192700b11c5a8a5c86c8b9fee4db0
Victor Glimskog <victor.glimskog@gmail.com>
2017-10-19 11:21:29 +0200

    Added a beginning to autenthication

--- app.js
+++ app.js
@@ -5,7 +5,30 @@ const session = require('express-session');
 
 const app = express();
 
+// Authenticaton middleware
+function auth(req, res, next) {
+    if (req.session && req.session.user && req.session.user.username === 'vgl') {
+        return next();
+    }
+    return res.sendStatus(401);
+}
+
 app.use(bodyParser.json());
+
+app.use(session({
+    secret: '&*$@&($fehfj343dhfkh334445ksd{',
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.post('/login', (req, res) => {
+    if (req.body.username === 'vgl' && req.body.password === 'plastapa') {
+        req.session.user = { username: req.body.username };
+        res.status(200).json({ msg: 'Successfully logged in' });
+    } else {
+        res.status(401).json({ msg: 'Incorrect credentials' });
+    }
+});
 
 app.use(RestSql.start({
     dbCredentials: {
@@ -19,11 +42,7 @@ app.use(RestSql.start({
     runtimeErrors: false,
 }));
 
-app.use(session({
-    secret: '&*$@&($fehfj343dhfkh334445ksd{',
-    resave: false,
-    saveUninitialized: true,
-}));
+app.get('/secretData', auth, (req, res) => res.status(200).send('Welcome, you are now logged in'));
 
 app.use(express.static('./www'));
 
