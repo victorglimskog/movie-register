2c071cbfb9d8b37c04c1c63bafa905523874032a
Kristoffer Annerfeldt <koffiman@me.com>
2017-10-30 09:08:48 +0100

    Added async delete function

--- src/rest-sql.class.js
+++ src/rest-sql.class.js
@@ -115,5 +115,34 @@ module.exports = class RestSql {
 		this.res.json(result);
 	
 	}
+
+    async delete() {
+
+        // Remove items
+        let remove = await this.query( 
+            'DELETE FROM' + this.table + '`' + (this.id ? ' WHERE' + this.title + ' =?' : ''),
+            [this.id]
+        );
+
+        // If error
+        if (result.constructor === Error) {
+          this.res.status(500);
+        }
+
+        // No post with a id could be found
+        else if (this.id && result.length === 0) {
+          this.res.status(500);
+          return;
+        }
+
+
+        // From array to object
+        else if(this.id) {
+          result = result[0];
+        }
+
+       // Return the reslut
+        this.res.json(result);
+    }
 };
 
